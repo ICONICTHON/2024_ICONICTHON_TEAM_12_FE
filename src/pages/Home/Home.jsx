4aa8a01528ed2abb92e072fe1214d1ed0be4aac5
kwdahun <kwdahun@naver.com>
2024-11-14 20:38:55 +0900

    Design: minor fix to Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -44,79 +44,91 @@ export default function Home() {
   );
 
   return (
-    <div className="box">
+    <div className="pt-32 pb-32">
+      <div className="box">
+        {/* Header Section */}
 
-            {/* Header Section */}
+        <header className="header">
+          {/* Replace TONE CONNECT text with logo image */}
+          <img
+            src={toneConnectLogo}
+            alt="TONE CONNECT"
+            className="TONE_CONNECT"
+          />
+          <img
+            src={arsTitle}
+            alt="보이는 ARS 쪽지 보내기"
+            className="ARS_logo"
+          />
+        </header>
 
-            <header className="header">
+        {/* Content Section */}
+        <section className="content">
+          {/* Recipients and Subject */}
+          <div className="input-group">
+            <div className="label">받는이</div>
+            <div className="input-box">
+              <input className="input-text" type="text" />
+            </div>
+          </div>
+          <div className="input-group">
+            <div className="label">제목</div>
+            <div className="input-box"></div>
+          </div>
 
-                {/* Replace TONE CONNECT text with logo image */}
-                <img src={toneConnectLogo} alt="TONE CONNECT" className="TONE_CONNECT" />
-                <img src={arsTitle} alt="보이는 ARS 쪽지 보내기" className="ARS_logo" />
-            </header>
+          {/* Keyword Selection */}
+          <div className="keywords-title">
+            키워드 선택
+            <button className="generate-button" onClick={handleClick}>
+              생성
+            </button>
+          </div>
 
+          <div className="flex gap-x-3 gap-y-2 flex-wrap">
+            {keywords.map((group, index) => {
+              const isGroupSelected = group.some((keyword) =>
+                selectedKeywords.includes(keyword)
+              );
 
-
-            {/* Content Section */}
-            <section className="content">
-                {/* Recipients and Subject */}
-                <div className="input-group">
-                    <div className="label">받는이</div>
-                    <div className="input-box"></div>
+              return (
+                <div
+                  className="flex gap-x-3 w-full flex-wrap"
+                  key={`keyword_group_${index}`}
+                >
+                  {group.map((keyword, i) => (
+                    <KeywordButton
+                      key={`keyword_${index}_${i}`}
+                      content={keyword}
+                      selectedKeywords={selectedKeywords}
+                      setSelectedKeywords={setSelectedKeywords}
+                      keywords={keywords}
+                      setKeywords={setKeywords}
+                      disabled={
+                        !selectedKeywords.includes(keyword) &&
+                        (isGroupSelected ||
+                          (firstEmptyGroupIndex !== -1 &&
+                            index > firstEmptyGroupIndex))
+                      }
+                    />
+                  ))}
                 </div>
-                <div className="input-group">
-                    <div className="label">제목</div>
-                    <div className="input-box"></div>
-                </div>
-        
-        {/* Keyword Selection */}
-        <div className="keywords-title">키워드 선택
-        <button className="generate-button" onClick={handleClick}>생성</button>
-        </div>
+              );
+            })}
+          </div>
 
-        <div className="flex gap-x-3 gap-y-2 flex-wrap">
-          {keywords.map((group, index) => {
-            const isGroupSelected = group.some((keyword) =>
-              selectedKeywords.includes(keyword)
-            );
+          {/* Text Area */}
 
-            return (
-              <div
-                className="flex gap-x-3 w-full flex-wrap"
-                key={`keyword_group_${index}`}
-              >
-                {group.map((keyword, i) => (
-                  <KeywordButton
-                    key={`keyword_${index}_${i}`}
-                    content={keyword}
-                    selectedKeywords={selectedKeywords}
-                    setSelectedKeywords={setSelectedKeywords}
-                    keywords={keywords}
-                    setKeywords={setKeywords}
-                    disabled={
-                      !selectedKeywords.includes(keyword) &&
-                      (isGroupSelected ||
-                        (firstEmptyGroupIndex !== -1 &&
-                          index > firstEmptyGroupIndex))
-                    }
-                  />
-                ))}
-              </div>
-            );
-          })}
-        </div>
-        
-        
-      
-      {/* Text Area */}
+          <textarea
+            className="text-area"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="텍스트가 여기 표시됩니다"
+          />
+          {/* Send Button */}
 
-      <textarea className="text-area" value={text} onChange={(e) => setText(e.target.value)} placeholder="텍스트가 여기 표시됩니다"/>
-{/* Send Button */}
-
-<button className="send-button">쪽지 보내기</button>
-    </section>
-  </div>
+          <button className="send-button">쪽지 보내기</button>
+        </section>
+      </div>
+    </div>
   );
 }
-
-
